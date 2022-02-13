aab2febb4aaf66b87a4c17b99a24b40ed29d4d67
DidiAbel <didiabel123@gmail.com>
2022-02-13 16:02:42 -0300

    ej9

--- Bucles/Ej 9/index.js
+++ Bucles/Ej 9/index.js
@@ -1,13 +1,19 @@
 let par =
   "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus";
-let letras = [];
 let counts = [];
 
 for (let i = 0; i < par.length; i++) {
-  letras.push({ nombre: par[i]});
-  for (let a = 0; a < letras.length; a++) {
-      
+  let cant = 0;
+  for (let a = 0; a < par.length; a++) {
+    
+    if (par[a].toLowerCase() === par[i].toLowerCase()) {
+      cant++;
+    }
   }
+  counts.push({
+    letra: par[i],
+    veces: cant,
+  });
 }
 
-console.log(letras);
+console.log(counts);
