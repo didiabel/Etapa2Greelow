e27cd508f6bbd0677614f727b9888fa0e4f79336
DidiAbel <didiabel123@gmail.com>
2022-03-03 18:15:53 -0300

    Updates

---
+++ precache-manifest.fc4dca3901039876f47f4ad1ff081a3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "12cbc8e973630084afef",
+    "url": "/Etapa2Greelow/static/css/main.57c225c8.chunk.css"
+  },
+  {
+    "revision": "12cbc8e973630084afef",
+    "url": "/Etapa2Greelow/static/js/main.12cbc8e9.chunk.js"
+  },
+  {
+    "revision": "269d177dfa7b1bf5e155",
+    "url": "/Etapa2Greelow/static/js/1.269d177d.chunk.js"
+  },
+  {
+    "revision": "e86474b4abcedbcc4a24",
+    "url": "/Etapa2Greelow/static/css/2.d71f42bd.chunk.css"
+  },
+  {
+    "revision": "e86474b4abcedbcc4a24",
+    "url": "/Etapa2Greelow/static/js/2.e86474b4.chunk.js"
+  },
+  {
+    "revision": "c3ec26fe4522cdc1b2e0",
+    "url": "/Etapa2Greelow/static/js/runtime~main.c3ec26fe.js"
+  },
+  {
+    "revision": "fc1551960b796b35311fdf6a050f86f0",
+    "url": "/Etapa2Greelow/static/media/Sin título(3).fc155196.jpg"
+  },
+  {
+    "revision": "6f3e1aea81e6080c1a84a809ae2cb44b",
+    "url": "/Etapa2Greelow/index.html"
+  }
+];
